63ec40bea06dfaf775464011c94816dcb2819889
Bhawna Bharat Mehbubani <bhawnamehbubani@gmail.com>
2023-02-25 18:24:57 +0530

    Experience Card done

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -16,11 +16,10 @@ export default function WorkExperience({}: Props) {
             Experience
         </h3>
 
-        <div className="w-full flex space-x-1 overflow-x-scroll p-10 snap-x snap-mandatory">
+        <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
             <ExperienceCard/>
-            <ExperienceCard/>
-            <ExperienceCard/>
-            <ExperienceCard/>
+
+
         </div>
 
         </motion.div>
